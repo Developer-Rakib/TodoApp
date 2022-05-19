6e243837639807b80c9f4f235001af67b2f8d4a3
Developer-Rakib <mdtazulislam6011@gmail.com>
2022-05-19 07:11:39 +0600

    done

--- src/Page/Home/Home.js
+++ src/Page/Home/Home.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+    const navigate = useNavigate()
     return (
-        <div>
-            this is home
+        <div className='flex justify-center h-[90vh] items-center'>
+            <div className=''>
+            <h1 className='text-4xl font-bold text-purple-500'>Todo App</h1>
+            <p className='py-2'>White Your Daily task</p>
+            <div class="flex space-x-2 justify-center">
+                <button
+                onClick={()=> navigate('/todoList')}
+                    type="button"
+                    class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                >Click me</button>
+            </div>
+        </div>
         </div>
     );
 };
--- src/Page/Todo/Todo.js
+++ src/Page/Todo/Todo.js
@@ -1,34 +1,86 @@
+import axios from 'axios';
 import React from 'react';
 import toast from 'react-hot-toast';
+import TodoList from './TodoList';
 
 const Todo = () => {
-    const handleTodo = (e)=>{
+    const handleTodo = (e) => {
         e.preventDefault()
         const todo = e.target.todo.value
+        const description = e.target.description.value
         if (todo === "") {
             toast.error("Please Enter Todo")
             return
         }
-        console.log(todo);
+        if (description === "") {
+            toast.error("Please Enter Desctiption")
+            return
+        }
+        const todoInfo = {
+            todo,
+            description
+        }
+        console.log(todoInfo);
+        axios.post(`https://intense-sierra-15615.herokuapp.com/todo`, todoInfo)
+            .then(data => {
+                console.log(data.data);
+                if (data.data.success) {
+                    toast.success(data.data.message)
+                }
+            })
         e.target.reset()
     }
     return (
-        <div class="flex justify-center">
-            <div class="mb-3 xl:w-96">
-                <div class="input-group relative flex flex-wrap items-stretch w-full mb-4">
+        <div className="flex items-center flex-col">
+            <div className="mb-3 xl:w-96">
+                <div className="input-group relative flex flex-wrap items-stretch w-full mb-4">
                     <form onSubmit={handleTodo} className='flex mt-16'>
-                        <input 
-                        name='todo'
-                        type="text" class="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Search" aria-label="Search" aria-describedby="button-addon2" 
-                        />
+                        <div className=''>
+                            <input
+                                name='todo'
+                                type="text" className="form-control mb-3 relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Enter Todo" aria-label="Search" aria-describedby="button-addon2"
+                            />
+                            <div className="flex justify-center">
+                                <div className="mb-3 xl:w-96">
+                                    <textarea
+                                        name='description'
+                                        className="
+                                        form-control
+                                        block
+                                        w-full
+                                        px-3
+                                        py-1.5
+                                        text-base
+                                        font-normal
+                                        text-gray-700
+                                        bg-white bg-clip-padding
+                                        border border-solid border-gray-300
+                                        rounded
+                                        transition
+                                        ease-in-out
+                                        m-0
+                                        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+                                    "
+                                        id="exampleFormControlTextarea1"
+                                        rows="2"
+                                        placeholder="Enter Description"
+                                    ></textarea>
+                                </div>
+                            </div>
+                        </div>
                         <button
-                            
-                            class="btn w-40 px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700  focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out flex items-center" type="submit" id="button-addon2">
+
+                            className="btn h-10 w-32 ml-5 px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700  focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out flex items-center" type="submit" id="button-addon2">
                             Add Todo
                         </button>
                     </form>
+
                 </div>
             </div>
+            <div className='w-full'>
+                <TodoList></TodoList>
+            </div>
+
         </div>
     );
 };
