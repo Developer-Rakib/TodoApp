ea15e2a9313c96c3f5d9d7ab486197322d97a397
Md Tazul Islam Rakib <91424644+Developer-Rakib@users.noreply.github.com>
2022-05-28 20:37:56 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,10 +17,12 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/todoList' element={
+
           <RequireAuth>
             <Todo></Todo>
           </RequireAuth>
         }></Route>
+
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/signUp' element={<SignUp></SignUp>}></Route>
       </Routes>
