860ea868cacf9190abbe664d6f1c61c641753d63
Developer-Rakib <mdtazulislam6011@gmail.com>
2022-06-30 22:20:19 +0600

    enter button added

--- src/Page/Header/Header.js
+++ src/Page/Header/Header.js
@@ -19,13 +19,13 @@ const Header = () => {
         setToggle(!toggle)
     }
 
-    const handleLogout = () => {
-        signOut(auth)
-            .then(() => {
-                navigat('/login')
-                toast.success('Logout Succes!')
-            })
-    }
+    // const handleLogout = () => {
+    //     signOut(auth)
+    //         .then(() => {
+    //             navigat('/login')
+    //             toast.success('Logout Succes!')
+    //         })
+    // }
 
     if (loading) {
         return <Loader></Loader>
@@ -39,8 +39,8 @@ const Header = () => {
                 <span onClick={navBtnHndle} className='md:hidden absolute right-6  top-[22px]'>{toggle ? <MdOutlineClose></MdOutlineClose> : <GoThreeBars></GoThreeBars>}</span>
                 <ul onClick={navBtnHndle} className={`md:flex md:justify-end z-10  md:flex-row left-0 md:left-0  md:relative md:opacity-100 md:w-full md:top-0 absolute top-12  py-4 md:py-0 duration-500 ${toggle ? "w-10/12 opacity-100" : "w-0 left-[-100px] opacity-0"}`}>
                     <NavLink className={({ isActive }) => (isActive ? 'activeLink' : 'navLink')} to={"/"}>Home</NavLink>
-                    <NavLink className={({ isActive }) => (isActive ? 'activeLink' : 'navLink')} to={"/completeTask"}>Completed Tasks</NavLink>
                     <NavLink className={({ isActive }) => (isActive ? 'activeLink' : 'navLink')} to={"/todoList"}>To-Do</NavLink>
+                    <NavLink className={({ isActive }) => (isActive ? 'activeLink' : 'navLink')} to={"/completeTask"}>Completed Tasks</NavLink>
                     <NavLink className={({ isActive }) => (isActive ? 'activeLink' : 'navLink')} to={"/calander"}>Calendar</NavLink>
                     {/* {user ?
                         <button onClick={handleLogout} className='uppercase my-0.5 md:my-0 text-left w-4/6 md:w-auto md:mx-0 mx-auto md:pb-0.5' >LogOut</button>
--- src/Page/Todo/Todo.js
+++ src/Page/Todo/Todo.js
@@ -1,81 +1,79 @@
 import axios from 'axios';
-import React from 'react';
+import React, { useState } from 'react';
 import toast from 'react-hot-toast';
 import TodoList from './TodoList';
 
 const Todo = () => {
-    const handleTodo = (e) => {
-        e.preventDefault()
-        const todo = e.target.todo.value
-        // const description = e.target.description.value
-        if (todo === "") {
-            toast.error("Please Enter Task")
+    const [addedTodo, setAddedTodo] = useState("")
+    // console.log(addedTodo);
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+
+            const todo = addedTodo
+            if (todo === "") {
+                toast.error("Please Enter Todo")
+                return
+            }
+            const todoInfo = {
+                todo,
+            }
+            console.log(todoInfo);
+            axios.post(`https://intense-sierra-15615.herokuapp.com/todo`, todoInfo)
+                .then(data => {
+                    console.log(data.data);
+                    if (data.data.success) {
+                        toast.success(data.data.message)
+                        event.target.value = "";
+                        setAddedTodo("")
+                    }
+                })
             return
         }
-        // if (description === "") {
-        //     toast.error("Please Enter Desctiption")
-        //     return
-        // }
-        const todoInfo = {
-            todo,
-            // description
-        }
-        console.log(todoInfo);
-        axios.post(`https://intense-sierra-15615.herokuapp.com/todo`, todoInfo)
-            .then(data => {
-                console.log(data.data);
-                if (data.data.success) {
-                    toast.success(data.data.message)
-                }
-            })
-        e.target.reset()
+
+        setAddedTodo(event.target.value)
     }
+
+    // const handleTodo = (e) => {
+
+    //     // if (e.keyCode === 13) {
+
+    //     // }
+    //     // e.preventDefault()
+    //     // const todo = e.target.todo.value
+    //     // const description = e.target.description.value
+    //     // if (todo === "") {
+    //     //     toast.error("Please Enter Todo")
+    //     //     return
+    //     // }
+    //     // if (description === "") {
+    //     //     toast.error("Please Enter Desctiption")
+    //     //     return
+    //     // }
+    //     // const todoInfo = {
+    //     //     todo,
+    //     //     description
+    //     // }
+    //     // console.log(todoInfo);
+    //     // axios.post(`https://intense-sierra-15615.herokuapp.com/todo`, todoInfo)
+    //     //     .then(data => {
+    //     //         console.log(data.data);
+    //     //         if (data.data.success) {
+    //     //             toast.success(data.data.message)
+    //     //         }
+    //     //     })
+    //     // e.target.reset()
+    // }
     return (
         <div className="flex items-center flex-col">
             <div className="mb-3 xl:w-96">
-                <div className="input-group relative flex flex-wrap items-stretch w-full mb-4">
-                    <form onSubmit={handleTodo} className='flex mt-16'>
-                        <div className=''>
-                            <input
-                                name='todo'
-                                type="text" className="form-control mb-3 relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Enter Todo" aria-label="Search" aria-describedby="button-addon2"
-                            />
-                            {/* <div className="flex justify-center">
-                                <div className="mb-3 xl:w-96">
-                                    <textarea
-                                        name='description'
-                                        className="
-                                        form-control
-                                        block
-                                        w-full
-                                        px-3
-                                        py-1.5
-                                        text-base
-                                        font-normal
-                                        text-gray-700
-                                        bg-white bg-clip-padding
-                                        border border-solid border-gray-300
-                                        rounded
-                                        transition
-                                        ease-in-out
-                                        m-0
-                                        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
-                                    "
-                                        id="exampleFormControlTextarea1"
-                                        rows="2"
-                                        placeholder="Enter Description"
-                                    ></textarea>
-                                </div>
-                            </div> */}
-                        </div>
-                        <button
-
-                            className="btn h-10 w-32 ml-5 px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700  focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out flex items-center" type="submit" id="button-addon2">
-                            Add Todo
-                        </button>
-                    </form>
-
+                <div className=' w-full mb-4 flex justify-center mt-16'>
+                    <input
+                        onKeyUp={handleKeyDown}
+                        name='todo'
+                        type="text" className="form-control mb-3 relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Enter Todo" aria-label="Search" aria-describedby="button-addon2"
+                    />
                 </div>
+
             </div>
             <div className='w-full'>
                 <TodoList></TodoList>
